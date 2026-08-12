00a540d5d4a63c8d4eb1fb8dc3d2157a91f61fea
nick12357 <101987755+nick12357@users.noreply.github.com>
2026-08-12 17:32:04 -0400

    Add Telemarketer Pylon game implementation
    Initial commit of the Telemarketer Pylon Procrastination Corner Quest Thingy game, including a series of questions and checkpoints based on telemarketing points.

---
+++ Like the actual code.py
@@ -0,0 +1,100 @@
+from logging import exception
+import time
+telemarketingpoints=0
+savenum=0
+def Checkpointmaker(telemarketingpoints):
+    if telemarketingpoints > 0:
+       print("You don't have enough telemarketer points for a save")
+
+    else:
+        telemarketingpoints =+ -100
+        savenum =+ 1
+
+
+print("===========================================================================")
+print("      Telemarketer Pylon Procrastination Corner Quest Thingy (TPPCQT) ")
+print("===========================================================================")
+print(">>>>>>>>>>>>>>>>V.1 or something>>>>>>>>>>>>>>>>>>>")
+time.sleep(2.5)
+boblebuilder=input("Press enter to continue or save to go to your last checkpoint")
+if boblebuilder  != "" or "save":
+  exception("just press enter or like save ik i could of made it continue but this is funner")
+
+if boblebuilder == "":
+  time.sleep(2.5)
+  print("...")
+  time.sleep(2.5)
+  print("Are you going to wait here?")
+  time.sleep(2.5)
+  print("We're getting late for our flight to Telemarketingland we have to go NOW")
+  time.sleep(2.5)
+  print("I hear you're a bit scared")
+  time.sleep(2.5)
+  print("But everything is going to be alright")
+  #for the record everything is not alright
+  time.sleep(2.5)
+  print("Let us hop aboard a plane")
+  time.sleep(2.5)
+  print("You will be OKAY")
+  time.sleep(2.5)
+  print("10 years later...")
+  time.sleep(5)
+  print("Hi, my name is generictelemarketernumber#1229 the Pylon, and this is MY story.")
+  print("I had the most telemarketing points at the University of Telemarketing (or Utelemarketing) until THE INCIDENT!")
+  print("What incident you may ask, well that’s the larp of 26.")
+  print("You see, as the top student at Telemarketing university, I lowkey had mad opps for real. ")
+  print("And one of these opps, well he was Devious Daniel. Devious Daniel was notorious for being devious, being named Daniel and worst of all, AN ARCH USER!")
+  print("This was because he got the arch user virus from a very young age which is unfortunately incurable.")
+  print("Devious Daniel did not like me. He had a poster on his wall the said, ‘Eat, Sleep, Mog generictelemarketernumber#1229, Repeat’")
+  print("Before I say this, let’s rewind for like 12 lines of code worth of story or smth idk I’m just in the writing department.")
+  print("Anyways let’s go back to 1 week ago, the biggest moment of any telemarketer's career.")
+  bobthebuilderpt2=input("type idc to continue or enter idk: ")
+  if bobthebuilderpt2 == "idc" or "":
+    print("Chapter 2: Examination 1 year earlier It’s the day of the entrance exam, the most stressful day of my life.")
+    print("Everything has led up to this. Telemarketing school entrance exams are the most important day of any students life.")
+    time.sleep(10)
+    print("It’s time. ")
+    print("You look at the questions and begin")
+    print("Question 1: Who is the best telemarketer? (CASE SENITIVE CAPITALS ONLY): ")
+    hi=input("A.Neekan (was number 2 at the time until winter of 25/26) \n B.Bob (the builder) (just bob) \n C.Dirpy (wasn't on the leaderboard at the time) \n D.Tung man 41 (wasn't a meme at the time) \n")
+    if hi == "B":
+      print("your correct 100 telemarketing points")
+      telemarketingpoints =+ 100
+    else:
+      print("-100 telemarketing points")
+      telemarketing =+ -100
+            ###############################################################################################################
+    print("NEXT QUESTION")
+    ilovegdcologne=input("Question 2: what is the best gym class item? \n A.Pool Noodle \n B.Hoola Hoop \n C.Pylon  \n D.John Chungus \n")
+    if ilovegdcologne == "C":
+      print("your correct 100 telemarketing points")
+      telemarketingpoints =+ 100
+    else:
+      print("-100 telemarketing points")
+      telemarketing =+ -100
+            ###############################################################################################################
+    inputvar=input("Question 3: What do you throw at Samar? \n A) Yogurt \n B) Samar popo uncle \n C) Clothes \n D) Apples \n")
+    if inputvar == "A":
+      print("your correct 100 telemarketing points")
+      telemarketingpoints =+ 100
+    else:
+      print("-100 telemarketing points")
+      telemarketing =+ -100
+      ###############################################################################################################
+inputvar=input("Question 4: what is the Telemarketing OS? \n A) UwUntu \n B) Arch \n C) Mint \n D) Windows \n")
+if inputvar == "C":
+      print("your correct 100 telemarketing points")
+      telemarketingpoints =+ 100
+else:
+      print("-100 telemarketing points")
+      telemarketing =+ -100
+Checkpointmaker(telemarketingpoints)
+if telemarketingpoints > 100
+:
+  print("you can continue")
+else:
+  raise exception ("You got rejected. Try again")
+print("Welcome to the open world of telemarketingland")
+
+
+
